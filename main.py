153f0457154560d1e7a690a9419b8ba944342749
cheese2611 <t.vdg@gmx.de>
2018-11-02 23:27:02 +0100

    major logic update, should be working by now! though design is very crappy now, this should be handeled very soon!

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-"""
-Starting Template
-
-Once you have learned how to use classes, you can begin your program with this
-template.
-
-If Python and Arcade are installed, this example can be run from the command line with:
-python -m arcade.examples.starting_template
-"""
 import arcade
 import numpy as np
 from PIL import Image
@@ -15,245 +6,289 @@ import threading
 import argparse
 from pubsub import pub
 import sys
+import time
+import os
 
-SCREEN_WIDTH = 1067
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 800
+
+def millis():
+	return int(round(time.time() * 1000))
 
 class Spaceship(arcade.Sprite):
-    def __init__(self, r, g, b, row):
-        fileName = "out/player_" + str(row) + ".png"
-        image = Image.open("spacecraft_white.png").convert('RGBA')
-        image_data = np.array(image)
-        image_data[(image_data == (255,255,255,255)).all(axis = -1)] = (r,g,b,255)
-        final_image = Image.fromarray(image_data, mode='RGBA')
-        final_image.save(fileName, "PNG")
+	def __init__(self, r, g, b, row):
+		fileName = 'out/player_' + str(r) + '_' + str(g) + '_' + str(b) + '.png'
+		if not os.path.isfile(fileName):
+			image = Image.open('spacecraft_white.png').convert('RGBA')
+			image_data = np.array(image)
+			image_data[(image_data == (255,255,255,255)).all(axis = -1)] = (r,g,b,255)
+			final_image = Image.fromarray(image_data, mode='RGBA')
+			final_image.save(fileName, 'PNG')
+		
+		super().__init__(fileName, 0.1)
+		self.center_x = 60
+		self.center_y = 50 + row * 50
+		self.angle = -90
+		self.color = (r, g, b)
+		
+		self.row = row
+		self.target_x = self.center_x
+	
+	def update(self):
+		if self.center_x < self.target_x:
+			self.velocity = [(self.target_x - self.center_x) / 10, 0]
+		else:
+			self.velocity = [0, 0]
+		super().update()
+	
+	def checkWinner(self):
+		if self.center_x > 860:
+			print('Winner: ' + str(self.row))
+			return self.row
+		else:
+			return -1
 
-        super().__init__(fileName, 0.1)
-        self.center_x = 60
-        self.center_y = 50 + row * 50
-        self.angle = -90
+class UnfinityGame(arcade.Window):
+	# Init, Setup & Update
+	def __init__(self, width, height):
+		super().__init__(width, height, fullscreen=False)
+		
+		arcade.set_background_color(arcade.color.AMAZON)
+		
+		self.shipList = None
+	
+	def setup(self, serialPort):
+		# Drawing
+		self.shipList = arcade.SpriteList()
+		
+		# Serial
+		self.serialPort = serialPort
+		self.serialBuffers = []
+		
+		print('Starting serial port thread', '...', end='')
+		serialThread = threading.Thread(target=self.readSerialData, args=(self.serialPort,))
+		serialThread.start()
+		print('done')
+		
+		# Start
+		self.initGame();
+	
+	def update(self, delta_time):
+		# Serial
+		while len(self.serialBuffers) > 3:
+			self.serialBuffers.pop(0)
+		if len(self.serialBuffers) > 0:
+			self.processSerialData(self.serialBuffers.pop(0))
+		
+		# Game
+		self.updateGame()
+		
+		# Drawing
+		if len(self.winners) == 0:
+			self.shipList.update()
+	
+	# Draw
+	def drawSFLine(self, x):
+		for y in range(0, SCREEN_HEIGHT, 20):
+			arcade.draw_rectangle_filled(x, y + 5, 10, 10, arcade.color.WHITE)
+			arcade.draw_rectangle_filled(x + 10, y + 5, 10, 10, arcade.color.BLACK)
+			arcade.draw_rectangle_filled(x, y + 15, 10, 10, arcade.color.BLACK)
+			arcade.draw_rectangle_filled(x + 10, y + 15, 10, 10, arcade.color.WHITE)
+	
+	# Game methods
+	def initGame(self):
+		self.gameState = 'INIT'
+		
+		self.playerCount = 0
+		self.winners = []
+		
+		self.countDown = 0
+		self.countDownTime = 0
+		
+		self.sendInit()
+	
+	def updateGame(self):
+		if self.gameState == 'COUNTDOWN':
+			if millis() - self.countDownTime > 1000:
+				self.countDownTime = millis()
+				self.countDown -= 1
+				if self.countDown == 0:
+					self.sendStart()
+					self.gameState = 'NORMAL'
+				print('COUNTDOWN:', self.countDown)
+	
+	# Ship methods
+	def deleteShips(self):
+		for ship in self.shipList:
+			ship.kill()
+		self.shipList = arcade.SpriteList()
+	
+	def printShips(self, rgbValues):
+		for index, colors in enumerate(rgbValues):
+			r = colors[0:3]
+			g = colors[3:6]
+			b = colors[6:9]
+			print('Colors:', r, g, b)
+			self.shipList.append(Spaceship(r, g, b, index))
+	
+	def moveShips(self, steps):
+		for index, step in enumerate(steps):
+			self.shipList[index].target_x += int(step) * 10
+	
+	# Callbacks
+	def on_draw(self):
+		arcade.start_render()
+		
+		self.drawSFLine(100)
+		self.drawSFLine(900)
+		
+		self.shipList.draw()
+		
+		if self.gameState == 'WAITING':
+			arcade.draw_text('Warte auf Spieler\nButton drücken zum beitreten', SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, arcade.color.WHITE, 20, align="center", anchor_x="center", anchor_y="center")
+		elif self.gameState == 'READY':
+			arcade.draw_text('Spielerzahl: ' + str(self.playerCount) + '\nButton drücken zum beitreten', SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, arcade.color.WHITE, 20, align="center", anchor_x="center", anchor_y="center")
+		elif self.gameState == 'COUNTDOWN':
+			arcade.draw_text(str(self.countDown) + '\nStarte Spiel mit ' + str(self.playerCount) + ' Spielern\n[BACKSPACE] drücken zum abbrechen', SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, arcade.color.WHITE, 20, align="center", anchor_x="center", anchor_y="center")
+		elif self.gameState == 'FINISHED':
+			arcade.draw_text('Gewonnen: ' + str(self.winners), SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, arcade.color.WHITE, 20, align="center", anchor_x="center", anchor_y="center")
+	
+	def on_key_press(self, key, key_modifiers):
+		if key == arcade.key.ENTER:
+			if self.gameState == 'READY':
+				self.gameState = 'COUNTDOWN'
+				self.countDown = 5
+				self.countDownTime = millis()
+				self.sendReady()
+				print('COUNTDOWN:', self.countDown)
+		elif key == arcade.key.BACKSPACE:
+			if self.gameState == 'COUNTDOWN':
+				self.gameState = 'READY'
+				self.sendWait()
+		elif key == arcade.key.ESCAPE:
+			arcade.window_commands.close_window()
+	
+	def on_key_release(self, key, key_modifiers):
+		pass
+	
+	def on_mouse_press(self, x, y, button, key_modifiers):
+		pass
+	
+	def on_mouse_motion(self, x, y, delta_x, delta_y):
+		pass
+	
+	def on_mouse_release(self, x, y, button, key_modifiers):
+		pass
+	
+	# Serial
+	def readSerialData(self, serialPort):
+		while True:
+			self.serialBuffers.append(serialPort.readline().decode('utf-8'));
+	
+	def processSerialData(self, serialLine):
+		serialLine = serialLine.rstrip('\n')
+		serialLine = serialLine.rstrip('\r')
+		print('Serial got "', serialLine, '": ', sep='', end='')
+		if serialLine == 'wait':
+			if self.gameState != 'INIT' and self.gameState != 'WAITING':
+				print('Got "wait" while not in state "INIT" or "WAITING"!')
+				return
+			print('waiting')
+			
+			if self.gameState == 'INIT':
+				self.gameState = 'WAITING'
+		elif serialLine.startswith('ready'):
+			if self.gameState != 'WAITING' and self.gameState != 'READY' and self.gameState != 'COUNTDOWN':
+				print('Got "wait" while not in state "WAITING" or "READY" or "COUNTDOWN"!')
+				return
+			
+			rgbValues = serialLine[6:].rstrip().split(',')
+			
+			for value in rgbValues:
+				if len(value) != 9:
+					print('Invalid RGB value', value)
+					return
+			
+			self.playerCount = len(rgbValues)
+			print('Player count =', self.playerCount)
+			
+			if self.gameState == 'WAITING':
+				self.gameState = 'READY'
+			
+			print('Deleting previous space ships')
+			self.deleteShips()
+			print('Printing space ships')
+			self.printShips(rgbValues)
+		elif serialLine.startswith('update'):
+			if self.gameState != 'NORMAL':
+				print('Got "update" while not in state "NORMAL"!')
+				return
+			
+			steps = serialLine[7:].rstrip().split(',')
+			
+			if len(steps) != self.playerCount:
+				print('Not enough or too many steps', '(', len(steps), '!=', self.playerCount, ')')
+				return
+			
+			print('Moving', steps, 'steps', '...', end='')
+			self.moveShips(steps)
+			print('done')
+			
+			# Check finished
+			for ship in self.shipList:
+				isWinner = ship.checkWinner()
+				if isWinner >= 0:
+					self.winners.append(isWinner)
+			
+			if len(self.winners) > 0:
+				self.gameState = 'FINISHED'
+				self.sendEnd()
+		else:
+			print('UNKOWN')
+	
+	def sendInit(self):
+		print('Sending \'init\'', '...', end="");
+		self.serialPort.write('init\n'.encode('utf-8'))
+		print('done');
+	
+	def sendReady(self):
+		print('Sending \'ready\'', '...', end="");
+		self.serialPort.write('ready\n'.encode('utf-8'))
+		print('done');
+	
+	def sendWait(self):
+		print('Sending \'wait\'', '...', end="");
+		self.serialPort.write('wait\n'.encode('utf-8'))
+		print('done');
+	
+	def sendStart(self):
+		print('Sending \'start\'', '...', end="");
+		self.serialPort.write('start\n'.encode('utf-8'))
+		print('done');
+	
+	def sendEnd(self):
+		print('Sending \'end\'', '...', end="");
+		self.serialPort.write('end '.encode('utf-8'))
+		for i in range(len(self.winners)):
+			if i > 0:
+				self.serialPort.write(','.encode('utf-8'))
+			self.serialPort.write(str(self.winners[i]).encode('utf-8'))
+		self.serialPort.write('\n'.encode('utf-8'))
+		print('done');
 
-        self.row = row
-        self.target_x = self.center_x
-    
-    def update(self):
-        if self.center_x < self.target_x:
-            self.velocity = [1, 0]
-        else:
-            self.velocity = [0, 0]
-        super().update()
-    
-    def checkWinner(self):
-        if self.center_x > 860:
-            print("Winner: " + str(self.row))
-            return self.row
-        else:
-            return False
-    
-    
-
-
-class MyGame(arcade.Window):
-    """
-    Main application class.
-
-    NOTE: Go ahead and delete the methods you don't need.
-    If you do need a method, delete the 'pass' and replace it
-    with your own code. Don't leave 'pass' in this program.
-    """
-    winner = []
-
-    def __init__(self, width, height):
-        super().__init__(width, height, fullscreen=False)
-        self.firstRun = True
-        
-
-        arcade.set_background_color(arcade.color.AMAZON)
-
-        # If you have sprite lists, you should create them here,
-        # and set them to None
-
-    def setup(self, arrayOfPlayerColors):
-        # Create your sprites and sprite lists here
-        self.ship_list = arcade.SpriteList()
-
-        for index, colors in enumerate(arrayOfPlayerColors):
-            r = colors[0:3]
-            g = colors[3:6]
-            b = colors[6:9]
-            print(colors, r,g,b)
-            ship = Spaceship(r,g,b, index)
-            self.ship_list.append(ship)        
-
-    def move(self, steps):
-        for index, step in enumerate(steps):
-            self.ship_list[index].target_x += int(step)
-
-
-    def on_draw(self):
-        """
-        Render the screen.
-        """
-
-        # This command should happen before we start drawing. It will clear
-        # the screen to the background color, and erase what we drew last frame.
-        arcade.start_render()
-
-        if self.firstRun:
-            self.firstRun = False
-            self.setFull()
-
-        self.drawSFLine(100)
-
-        self.drawSFLine(900)
-
-        # Call draw() on all your sprite lists below
-        self.ship_list.draw()
-
-        if len(self.winner) > 0:
-            arcade.draw_text("Gewonnen: " + str(self.winner), 400, 250, arcade.color.WHITE, 20)
-            
-
-    def update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        Normally, you'll call update() on the sprite lists that
-        need it.
-        """
-        if len(self.winner) == 0:
-            self.ship_list.update()
-
-            for ship in self.ship_list:
-                isWinner = ship.checkWinner()
-                if isWinner is not False:
-                    self.winner.append(isWinner)
-            if len(self.winner) > 0:
-                pub.sendMessage('gameState.winners', winners=self.winner)
-
-    def on_key_press(self, key, key_modifiers):
-        """
-        Called whenever a key on the keyboard is pressed.
-
-        For a full list of keys, see:
-        http://arcade.academy/arcade.key.html
-        """
-        if key == arcade.key.LEFT:
-            self.ship_list[0].velocity = [1,0]
-        elif key == arcade.key.ESCAPE:
-            arcade.window_commands.close_window()
-
-    def on_key_release(self, key, key_modifiers):
-        """
-        Called whenever the user lets off a previously pressed key.
-        """
-        self.ship_list[0].velocity = [0,0]
-
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """
-        Called whenever the mouse moves.
-        """
-        pass
-
-    def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        pass
-
-    def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when a user releases a mouse button.
-        """
-        pass
-
-    def drawSFLine(self, x):
-        for y in range(0, SCREEN_HEIGHT, 20):
-            arcade.draw_rectangle_filled(x, y + 5, 10, 10, arcade.color.WHITE)
-            arcade.draw_rectangle_filled(x + 10, y + 5, 10, 10, arcade.color.BLACK)
-            arcade.draw_rectangle_filled(x, y + 15, 10, 10, arcade.color.BLACK)
-            arcade.draw_rectangle_filled(x + 10, y + 15, 10, 10, arcade.color.WHITE)
-
-    def setFull(self):
-        self.set_viewport(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT)
-
-
-
-class Unfinity():
-    gamestate = "setup"
-
-    def handle_data(self, serialLine):
-        print(serialLine)
-        if serialLine.startswith('ready') and self.gamestate == "setup":
-            print("Start")
-            parts = serialLine.rstrip().split(' ')
-            if len(parts) == 2:
-                rgbValues = parts[1].split(',')
-                if len(rgbValues) == self.players:
-                    invalidData = False
-                    for values in rgbValues:
-                        if len(values) % 9 != 0:
-                            print("Invalid RGB value", values)
-                            invalidData = True
-                    if not invalidData:
-                        gameThread = threading.Thread(target=self.startGame, args=(rgbValues,))
-                        gameThread.start()
-                else:
-                    print("Not enough or too many rgb values")
-            else:
-                print("Invalid command", serialLine)
-        elif serialLine.startswith('m:'):
-            print("Move")
-            steps = serialLine[2:].rstrip().split(',')
-            if len(steps) == self.players:
-                self.game.move(steps)
-            else:
-                print("Not enoguh or too many steps")
-            print(steps)
-        else:
-            print(serialLine)
-            
-
-    def readSerial(self, serialPort):
-        while True:
-            reading = serialPort.readline().decode()
-            self.handle_data(reading)
-
-    def startGame(self, rgbValues):
-        self.game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
-
-        self.game.setup(rgbValues)
-        self.game.setFull()
-        arcade.run()
-
-    def sendWinners(self, winners):
-        winnerString = 'end ' + ','.join(str(w) for w in winners)
-        print(winnerString)
-        self.serialPort.write(winnerString.encode())
-        
-
-    def main(self):
-        """ Main method """
-        parser = argparse.ArgumentParser()
-        parser.add_argument('port')
-        parser.add_argument('players', type=int)
-
-        args = parser.parse_args()
-
-        port = args.port
-        self.players = args.players
-
-        self.serialPort = serial.Serial(port)
-        self.serialPort.write(b'init %d \n' % self.players)
-        
-        serialThread = threading.Thread(target=self.readSerial, args=(self.serialPort,))
-        serialThread.start()
-
-        pub.subscribe(self.sendWinners, 'gameState.winners')
-
-
-
-
-if __name__ == "__main__":
-    uf = Unfinity()
-    uf.main()
-    
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('port')
+	args = parser.parse_args()
+	
+	serialPortName = args.port
+	print('Serial port:', serialPortName)
+	
+	print('Opening serial port', '...', end='')
+	serialPort = serial.Serial(serialPortName, 115200)
+	print('done')
+	
+	game = UnfinityGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+	game.setup(serialPort)
+	game.set_viewport(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT)
+	arcade.run()
